aae47953f922f7a6abb63f7a831d5ed9d40ed4f0
gbn786 <gbn786@gmail.com>
2022-05-27 21:29:12 +0530

    anagram of strings

--- src/com/rc/test/Anagram.java
+++ src/com/rc/test/Anagram.java
@@ -4,50 +4,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- God = Dog
-
- Listen = silent
+ * God = Dog
+ * <p>
+ * Listen = silent
  */
 public class Anagram {
 
     public static void main(String[] args) {
         String input1 = "listen";
         String input2 = "silennt";
-        boolean isAnagram = checkAnagram(input1,input2);
-        System.out.println(isAnagram==true?"Anagram":"Not");
+        boolean isAnagram = checkAnagram(input1, input2);
+        System.out.println(isAnagram == true ? "Anagram" : "Not");
 
     }
 
-    private static boolean checkAnagram(String stra, String strb) {
-//        d->1,
-//        o=>1,
-//        g->1;
-        //Map of inputA
-        Map<Character,Integer> inputMap = new HashMap<>();
-
-        char[] inArray = stra.toCharArray();
-        char[] inArray2 = strb.toCharArray();
-
-
-        for (int i=0; i<inArray.length; i++) {
-            if(inputMap.get(inArray[i])!=null) {
-                inputMap.put(inArray[i], inputMap.get(inArray[i])+1);
+    private static boolean checkAnagram(String input1, String input2) {
+        if (input1.length() != input2.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < input1.length(); i++) {
+            char c = input1.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
             } else {
-                inputMap.put(inArray[i],1);
+                map.put(c, 1);
             }
         }
-        //System.out.println(inputMap);
-
-        for (int i=0; i<inArray2.length; i++) {
-            if (inputMap.get(inArray2[i])==null || inputMap.get(inArray2[i])==0) {
+        for (int i = 0; i < input2.length(); i++) {
+            char c = input2.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+            } else {
+                map.put(c, -1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
                 return false;
-            } else {
-                inputMap.put(inArray2[i], inputMap.get(inArray2[i])-1);
             }
         }
-
-        //loop through strB and check if present in Map
         return true;
     }
+
 }
